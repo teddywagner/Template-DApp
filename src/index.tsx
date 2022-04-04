b705f8baccca9cb02f399af2e19980046835f2af
Teddy Wagner <nicholas.e.wagner@vanderbilt.edu>
2022-04-04 11:47:31 -0500

    template

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { MoralisProvider } from "react-moralis";
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider serverUrl="https://ngby0hxyi8bz.usemoralis.com:2053/server" appId="GNWxhGTEgXSLGSkW7KkJ5CIDAFMOXYqghKYut6na">
+
       <App />
-    </MoralisProvider>
+
   </React.StrictMode>,
   document.getElementById("root")
 );
