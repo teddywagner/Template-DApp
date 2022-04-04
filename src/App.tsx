18cd63efbd3cc9a3f9fb823631ca4f79fb3bb263
Teddy Wagner <nicholas.e.wagner@vanderbilt.edu>
2022-04-04 11:41:25 -0500

    template

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,17 @@ function App() {
     const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
 
     const login = async () => {
-      if (!isAuthenticated) {
-
-        await authenticate({signingMessage: "Log in using Moralis" })
-          .then(function (user) {
-            console.log("logged in user:", user);
-            console.log(user!.get("ethAddress"));
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
+      
     }
 
     const logOut = async () => {
-      await logout();
-      console.log("logged out");
+      
     }
 
     // Render Metamask authentication
   return (
     <div>
+      <h1>Metamask Authentication with Moralis</h1>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// Import the MoralisProvider component
+import { MoralisProvider } from "react-moralis";
 
-// Attach the component to the page
+
 ReactDOM.render(
   <React.StrictMode>
+    <MoralisProvider serverUrl="https://ngby0hxyi8bz.usemoralis.com:2053/server" appId="GNWxhGTEgXSLGSkW7KkJ5CIDAFMOXYqghKYut6na">
       <App />
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
